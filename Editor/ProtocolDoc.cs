e40608d4a8716333c4ad8b04d2978349f7d22931
Seantong <jacquestx1@163.com>
2020-01-13 17:15:38 +0800

    Editor\ProtocolDoc.cs

--- Editor/ProtocolDoc.cs
+++ Editor/ProtocolDoc.cs
@@ -1,10 +1,10 @@
-﻿using EditorPlus;
+using EditorPlus;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CustomSeanLibEditor("Protocol")]
+[CustomSeanLibEditor("Protocol", IsDoc = true)]
 public class ProtocolDoc : EditorMarkDownWindow
 {
-    public override string RelativePath => "../../Doc";
     public override bool SearchField => true;
+    public override bool EditScript => false;
 }
